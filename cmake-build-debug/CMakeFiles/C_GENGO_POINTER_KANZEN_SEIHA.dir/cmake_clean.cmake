22b32704c6f9e2723270fbf5799321bf5dae24ab
manfred3431@outlook.com <manfred3431@outlook.com>
2019-08-27 08:16:01 +0800

    First

---
+++ cmake-build-debug/CMakeFiles/C_GENGO_POINTER_KANZEN_SEIHA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_GENGO_POINTER_KANZEN_SEIHA.dir/main.c.obj"
+  "C_GENGO_POINTER_KANZEN_SEIHA.pdb"
+  "C_GENGO_POINTER_KANZEN_SEIHA.exe"
+  "C_GENGO_POINTER_KANZEN_SEIHA.exe.manifest"
+  "libC_GENGO_POINTER_KANZEN_SEIHA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_GENGO_POINTER_KANZEN_SEIHA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/C_GENGO_POINTER_KANZEN_SEIHA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_GENGO_POINTER_KANZEN_SEIHA.
+# This may be replaced when dependencies are built.
